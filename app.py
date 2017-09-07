dce05dc4b6d726a0999a126869ce86169b1886cb
Hifza Sakhi <hifzasakhi@gmail.com>
2017-09-07 00:30:36 -0400

    adding and finding inv to db; experimenting with ui templates

--- app.py
+++ app.py
@@ -1,31 +1,81 @@
-from PIL import Image
 from flask import Flask, request, render_template
 from jinja2 import Template, Environment, PackageLoader, select_autoescape
 import io
-import urllib2 as urllib2 
-import urllib 
-import random
-import string
+import urllib2, urllib, random, string  
 import pymongo
 from pymongo import MongoClient
-
-
+from pprint import pprint
 import json
-from PIL import Image
-from urllib2 import urlopen
       
 app = Flask(__name__)
+inventory = None
+db = None
 
 def renderTemplate(type):
   if type == "Login":
     print("Rendering login page template")
     return render_template("login.html")
+  elif type == "Error":
+    return render_template("error.html")
 
+def validCredentials():
+  
+  return True
 
+def getInventoryDBConnection():
+  try:
+
+    client = MongoClient('localhost', 27017)
+    db = client.Inventory
+    return db
+
+  except Exception, e:
+    print str(e)
+
+# def getUsersDBConnection():
+#   try:
+
+#     client = MongoClient('localhost', 27017)
+#     db = client.Users
+#     return db
+
+#   except Exception, e:
+#     print str(e)
+
+def insertInventory(price, name, qty, pic=None, description=None):
+  try:
+    db.Inventory.insert_one(
+      {
+        "price": price,
+        "name": name,
+        "quantity": qty,
+        "pic": pic,
+        "description": description
+      })
+
+  except Exception, e:
+    print str(e)
+
+def printInventory():
+  collection = db.Inventory
+  cursor = collection.find({})
+  for document in cursor: 
+    print(document)
+  
 @app.route('/login',methods=['GET'])
 def login():
+  if validCredentials():
+    return renderTemplate("Login")
+  else:
+    return renderTemplate("Error")     
 
-  return renderTemplate("Login")
+
 
 if __name__ == "__main__":
-  app.run(port=8000,debug=True)
+  
+  db = getInventoryDBConnection()
+  insertInventory(20,"Apple",1)
+  insertInventory(22,"Orange",2)
+  insertInventory(24,"Banana",1)
+  printInventory()
+  #app.run(port=8000,debug=True)
